62b44909d3a2aade8abfaffc6163d0b705a63d44
Jacob Patterson <jacob.patterson@gmail.com>
2020-10-26 10:15:48 -0700

    fixing test to work with other timezones

--- go/ui/dom_test.go
+++ go/ui/dom_test.go
@@ -1,8 +1,22 @@
+// +build js,wasm
+
 package ui
 
-import "testing"
+import (
+	"syscall/js"
+	"testing"
+)
 
 func TestFormatTime(t *testing.T) {
+	tzoFunc := js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		return 420 // 7 hours past UTC
+	})
+	defer tzoFunc.Release()
+	datePrototype := js.Global().Get("Date").Get("prototype")
+	getTimezoneOffset := "getTimezoneOffset"
+	origFunc := datePrototype.Get(getTimezoneOffset)
+	defer datePrototype.Set(getTimezoneOffset, origFunc)
+	datePrototype.Set(getTimezoneOffset, tzoFunc)
 	utcSeconds := int64(1603729564)
 	want := "09:26:04"
 	got := FormatTime(utcSeconds)
